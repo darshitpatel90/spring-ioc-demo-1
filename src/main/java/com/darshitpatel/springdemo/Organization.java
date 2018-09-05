898e064deeeb9b775f11d7a406a7d6124030ff38
Darshitkumar Patel <dpate243@uwo.ca>
2018-09-05 19:02:14 -0400

    First Commit

---
+++ src/main/java/com/darshitpatel/springdemo/Organization.java
@@ -0,0 +1,10 @@
+package com.darshitpatel.springdemo;
+
+public class Organization {
+	
+	public void corporateSlogan(){
+		String slogan = "We build the utlimate driving machines.";
+		System.out.println(slogan);
+	}
+
+}
